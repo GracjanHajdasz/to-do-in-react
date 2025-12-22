eafa379dbc8972fd96649e94569844a2f91b2733
Gracjan Hajdasz <b3nny3@wp.pl>
2025-12-22 16:43:17 +0100

    screenshots

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,36 @@
-import { useState } from 'react'
-import { Task } from './components/Task'
-import './App.css'
+import { useState } from "react";
+import { Task } from "./components/task.jsx";
+import "./App.css";
 
 function App() {
   const [allTasks, setAllTasks] = useState([]);
   const [task, setTask] = useState("");
 
   function addTask() {
-    const newTasks = [
-      ...allTasks,
-      { task: task, id: crypto.randomUUID() }
-    ]
+    const newTasks = [...allTasks, { task: task, id: crypto.randomUUID() }];
     setAllTasks(newTasks);
     setTask("");
   }
-  
+
   function saveTask(event) {
     setTask(event.target.value);
   }
 
   function deleteTask(id) {
-    setAllTasks(allTasks.filter((task) => task.id !== id))    
+    setAllTasks(allTasks.filter((task) => task.id !== id));
   }
 
   return (
     <div className="container">
       <h1>Todo</h1>
       <input
-        value={task} 
-        onChange={saveTask} 
-        type="text" 
+        value={task}
+        onChange={saveTask}
+        type="text"
         placeholder="Add a new task..."
         onKeyDown={(event) => {
-          if(event.key === 'Enter')
-            addTask();
-          else if(event.key === 'Escape')
-            setTask("");  
+          if (event.key === "Enter") addTask();
+          else if (event.key === "Escape") setTask("");
         }}
       />
       <div className="controls">
@@ -46,7 +41,7 @@ function App() {
       {console.log(allTasks)}
       <Task tasks={allTasks} deleteTask={deleteTask} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
